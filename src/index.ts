76164fd87756294e49f020b6492f57f9dd0a1f62
Tupalski Ilya <itupalski@gmail.com>
2024-03-01 15:09:13 +0100

    feat: add structure project and packages (Fri Mar  1 15:09:13 CET 2024)

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,6 @@
 import express, { Express, Request, Response } from "express";
+import cookieParser from "cookie-parser";
+import cors from "cors";
 import dotenv from "dotenv";
 
 dotenv.config();
@@ -7,6 +9,15 @@ const PORT = process.env.PORT || 4000;
 
 const app: Express = express();
 
+app.use(express.json());
+app.use(cookieParser());
+app.use(
+    cors({
+        credentials: true,
+        origin: process.env.CLIENT_URL,
+    }),
+);
+
 app.get("/", (req: Request, res: Response) => {
     res.send("Hello this is api server");
 });
@@ -14,3 +25,5 @@ app.get("/", (req: Request, res: Response) => {
 app.listen(PORT, () => {
     console.log(`Start server on port ${PORT}`);
 });
+
+export default app;
