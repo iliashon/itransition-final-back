5fe2a3a6ea109f758823d56d07248db193cf7168
Tupalski Ilya <itupalski@gmail.com>
2024-03-01 12:19:59 +0100

    feat: add base packages and configurate typescript (Fri Mar  1 12:19:59 CET 2024)

---
+++ src/index.ts
@@ -0,0 +1,16 @@
+import express, { Express, Request, Response } from "express";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const PORT = process.env.PORT || 4145;
+
+const app: Express = express();
+
+app.get("/", (req: Request, res: Response) => {
+    res.send("Hello this is api server");
+});
+
+app.listen(PORT, () => {
+    console.log(`Start server on port ${PORT}`);
+});
